4976c384382f926e72686b4fade001fee9ec953f
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2020-06-04 19:35:09 +0100

    fixed inconsistency in polygon offsetting

--- PolygonOffsetTool.gd
+++ PolygonOffsetTool.gd
@@ -10,19 +10,36 @@ func handler(_b):
 		var node: Polygon2D = get_node(target)
 		node.set_polygon(offset_polygon(node.get_polygon(), offset_width))
 		
+func path_points_to_vectors(path: PoolVector2Array, path_closed: bool) -> PoolVector2Array:
+	var vec_path: PoolVector2Array = PoolVector2Array()
+	for i in range(path.size()-1):
+		vec_path.append(path[i+1]-path[i])
+	if path_closed:
+		vec_path.append(path[0]-path[path.size()-1])
+	return vec_path
+		
+func poly_points_clockwise(poly: PoolVector2Array) -> bool:
+	var vec_path: PoolVector2Array = path_points_to_vectors(poly, true)
+	var rotation: float = 0
+	for i in range(vec_path.size()):
+		rotation += vec_path[i].angle_to(vec_path[posmod(i+1, vec_path.size())])
+	return rotation >= 0
+		
 func offset_polygon(poly: PoolVector2Array, offset: float) -> PoolVector2Array:
+	var cw: bool = poly_points_clockwise(poly)
 	var poly_size: int = poly.size()
 	var new_poly: PoolVector2Array = poly
-	var ac_vec: Vector2
 	var vec: Vector2
-	var c_vec: Vector2
+	var prev_vec: Vector2
+	var next_vec: Vector2
 	var dir: Vector2
 	var mag: float
 	for i in range(poly_size):
 		vec = poly[i]
-		ac_vec = poly[posmod(i+1, poly_size)]
-		c_vec = poly[posmod(i-1, poly_size)]
-		dir = ((vec - ac_vec).normalized() + (vec - c_vec).normalized()).normalized()
-		mag = offset/sin((vec - ac_vec).normalized().angle_to(dir))
-		new_poly.set(i, new_poly[i] + dir*mag)
+		prev_vec = poly[posmod(i - (1 if cw else -1), poly_size)]
+		next_vec = poly[posmod(i + (1 if cw else -1), poly_size)]
+		dir = ((vec - prev_vec).normalized() + (vec - next_vec).normalized()).normalized()
+		mag = offset/sin((vec - prev_vec).normalized().angle_to(dir))
+		new_poly.set(i, new_poly[i] - dir*mag)
 	return new_poly
+	

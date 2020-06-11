aac8d817861bd4f33b1b7d62c7c9360ad57aee82
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2020-06-11 01:33:39 +0100

    added path gen code

--- scripts/ArenaDrawTool.gd
+++ scripts/ArenaDrawTool.gd
@@ -4,11 +4,11 @@ extends Node2D
 export var arena_size: float = 2048
 export var arena_color: Color = Color(0,0.5,0)
 
-export var river_width: float = 250
+export var river_width: float = 256
 export var river_color: Color = Color(0,0,1)
 
-export var lane_width: float = 250
-export var lane_curve: float = 500
+export var lane_width: float = 256
+export var lane_curve: float = 512
 export var lane_color: Color = Color(0.7,0.7,0)
 export var corner_color: Color = Color(0,0,0)
 
@@ -18,8 +18,11 @@ export var team2_color: Color = Color(0.5, 0.5, 1)
 
 export var fountain_radius: float = 128
 export var fountain_color: Color = Color(0.75,0.75,0.75)
+
+export var path_radius: float = 128
 	
 export var draw: bool = false setget drawH
+export var make_paths: bool = false setget make_paths
 
 func drawH(_b):
 	if Engine.is_editor_hint():
@@ -54,7 +57,6 @@ func _draw() -> void:
 	#fountains
 	draw_circle(Vector2(0, arena_size), fountain_radius, fountain_color)
 	draw_circle(Vector2(arena_size, 0), fountain_radius, fountain_color)
-	
 
 func draw_circle_arc_poly(center, inner_radius, outer_radius, angle_from, angle_to, color):
 	var nb_points = 32
@@ -70,3 +72,40 @@ func draw_circle_arc_poly(center, inner_radius, outer_radius, angle_from, angle_
 	else:
 		points_arc.push_back(center)
 	draw_polygon(points_arc, colors)
+
+func make_paths(_b) -> void:
+	for node in get_children():
+		remove_child(node)
+	
+	#https://stackoverflow.com/questions/1734745/how-to-create-circle-with-b%C3%A9zier-curves
+	var magic_number: float = 0.552284749831
+		
+	var top_curve: Curve2D = Curve2D.new()
+	top_curve.add_point(Vector2(lane_width/2, arena_size - lane_width/2 - path_radius))
+	top_curve.add_point(Vector2(lane_width/2, lane_curve), Vector2.ZERO, Vector2(0, -(lane_curve-lane_width/2)*magic_number))
+	top_curve.add_point(Vector2(lane_curve, lane_width/2), Vector2(-(lane_curve-lane_width/2)*magic_number, 0), Vector2.ZERO)
+	top_curve.add_point(Vector2(arena_size - lane_width/2 - path_radius, lane_width/2))
+	add_curve("Top", top_curve)
+	
+	var mid_curve: Curve2D = Curve2D.new()
+	var mr: float = path_radius/sqrt(2)
+	mid_curve.add_point(Vector2(lane_width/2 + mr, arena_size - lane_width/2 - mr))
+	mid_curve.add_point(Vector2(arena_size - lane_width/2 - mr, lane_width/2 + mr))
+	add_curve("Mid", mid_curve)
+	
+	var bot_curve: Curve2D = Curve2D.new()
+	bot_curve.add_point(Vector2(lane_width/2 + path_radius, arena_size - lane_width/2))
+	bot_curve.add_point(Vector2(arena_size - lane_curve, arena_size - lane_width/2), Vector2.ZERO, Vector2((lane_curve-lane_width/2)*magic_number, 0))
+	bot_curve.add_point(Vector2(arena_size - lane_width/2, arena_size - lane_curve), Vector2(0, (lane_curve-lane_width/2)*magic_number), Vector2.ZERO)
+	bot_curve.add_point(Vector2(arena_size - lane_width/2, lane_width/2 + path_radius))
+	add_curve("Bot", bot_curve)
+	
+func add_curve(name: String, curve: Curve2D):
+	var path: Path2D = Path2D.new()
+	path.set_curve(curve)
+	path.name = name
+	add_child(path)
+	path.set_owner(get_tree().get_edited_scene_root())
+	var follow: PathFollow2D = PathFollow2D.new()
+	path.add_child(follow)
+	follow.set_owner(get_tree().get_edited_scene_root())
